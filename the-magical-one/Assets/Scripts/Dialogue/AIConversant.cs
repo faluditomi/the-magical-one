8bcaee67e1afa35e4dd6c3b0875cc60d5fb67864
Andrei Mataoanu <amatao1999@gmail.com>
2025-05-31 13:49:02 +0300

    Dialogue?

---
+++ the-magical-one/Assets/Scripts/Dialogue/AIConversant.cs
@@ -0,0 +1,299 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+
+public class AIConversant : MonoBehaviour
+{
+    #region Attributes
+    public Dialogue dialogue;
+    private Dialogue currentDialogue;
+    private DialogueNode currentNode;
+    //private PlayerCombat playerCombat;
+
+    private Transform dialogueBubblePosition;
+
+    private GameObject dialogueBubbleGameObject;
+
+    //private OutlineMaterial outlineMaterial;
+
+    private Image dialoguePanelText;
+
+    private bool isDialoguing;
+    private bool isEarlyQuit;
+
+    private int index;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        dialoguePanelText = GameObject.Find("DialogueFrame").GetComponent<Image>();
+
+        dialogueBubblePosition = GetComponentInChildren<Transform>().Find("DialogueBoxPosition");
+
+        dialogueBubbleGameObject = GameObject.Find("DialogueBubble");
+
+        //playerCombat = FindObjectOfType<PlayerCombat>();
+
+        //outlineMaterial = GetComponent<OutlineMaterial>();
+    }
+        
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && isDialoguing)
+        {
+            StartCoroutine(EarlyQuitDialogue());
+        }
+    }
+    #endregion
+
+    #region Normal Methods
+    //Changes the current node to the next one.
+    private void Next()
+    {
+        TriggerActions();
+
+        currentNode = (index < currentDialogue.GetAllNodesByIndex()) ? currentDialogue.GetNodeByIndex(index) : currentDialogue.GetNodeByIndex(index - 1);
+    }
+
+    //Checks if there are anymore nodes.
+    private bool HasNext()
+    {
+        return (index <= currentDialogue.GetAllNodesByIndex() - 1);
+    }
+
+    //Triggers actions, if there are any, when dialoguing. 
+    private void TriggerActions()
+    {
+        if(currentNode != null && currentNode.GetTriggerActions().Count > 0)
+        {
+            foreach(DialogueTrigger trigger in this.GetComponents<DialogueTrigger>())
+            {
+                trigger.Trigger(currentNode.GetTriggerActions());
+            }
+        }
+    }
+    #endregion
+
+    #region Setters
+    public void SetNewDialogue(Dialogue newDialogue)
+    {
+        if(newDialogue != null)
+        {
+            dialogue = newDialogue;
+        }
+    }
+    #endregion
+
+    #region Coroutines
+    public IEnumerator RunDialogue()
+    {
+        if(dialogue != null)
+        {
+            if(!isDialoguing)
+            {
+                //playerCombat.StopCombo();
+
+                dialogueBubbleGameObject.transform.position = dialogueBubblePosition.position;
+
+                StartCoroutine(FadeInImageBehaviour(dialoguePanelText));
+
+                isDialoguing = true;
+                    
+                StartCoroutine(StartDialogue(dialogue));
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            StartCoroutine(UpdateUI());
+
+            yield return new WaitForEndOfFrame();
+                
+            if(HasNext())
+            {
+                Next();
+
+                index++;
+
+                yield return new WaitForEndOfFrame();
+            }
+            else
+            {
+                yield return new WaitForEndOfFrame();
+
+                StartCoroutine(EarlyQuitDialogue());
+            }
+        }
+    }
+
+    private IEnumerator StartDialogue(Dialogue newDialogue)
+    {
+        yield return new WaitForEndOfFrame();
+
+        index = 0;
+
+        currentDialogue = newDialogue;
+
+        currentNode = currentDialogue.GetNodeByIndex(index);
+
+        //PlayerStateController.SetIsInDialogue(true);
+
+        isEarlyQuit = false;
+
+        StartCoroutine(FadeInBehaviour(GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>()));
+    }
+
+    public IEnumerator QuitDialogue()
+    {
+        if(!isEarlyQuit)
+        {
+            yield return new WaitForEndOfFrame();
+
+            GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>().text = "";
+
+            GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>().color = new Color(GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>().color.r, GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>().color.g, GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>().color.b, 0f);
+
+            isDialoguing = false;
+
+            StartCoroutine(FadeOutImageBehaviour(dialoguePanelText));
+                
+            //outlineMaterial.SetDefaultMaterial();
+
+            currentDialogue = null;
+
+            currentNode = null;
+
+            //PlayerStateController.SetIsInDialogue(false);
+
+            index = 0;
+
+            isEarlyQuit = true;
+        }
+    }
+
+    //Temporary. I can't think about it right now. Need an early quit for a bux fix but I can't remember what the bug was. Will remove after we send the demo.
+    public IEnumerator EarlyQuitDialogue()
+    {
+        yield return new WaitForEndOfFrame();
+
+        GameObject find = GameObject.Find(currentNode.dialogueTextGameObject);
+
+        if(find != null)
+        {
+            find.GetComponent<TMP_Text>().text = "";
+        }
+
+        GameObject o = GameObject.Find(currentNode.speakerTextGameObject);
+
+        if(o != null)
+        {
+            o.GetComponent<TMP_Text>().text = "";
+
+            o.GetComponent<TMP_Text>().color = new Color(o.GetComponent<TMP_Text>().color.r,
+                o.GetComponent<TMP_Text>().color.g, o.GetComponent<TMP_Text>().color.b, 0f);
+        }
+
+        isDialoguing = false;
+
+        StartCoroutine(FadeOutImageBehaviour(dialoguePanelText));
+            
+        //outlineMaterial.SetDefaultMaterial();
+
+        currentDialogue = null;
+
+        currentNode = null;
+
+        //PlayerStateController.SetIsInDialogue(false);
+
+        index = 0;
+
+        isEarlyQuit = true;
+    }
+
+    //Updates the UI text.
+    private IEnumerator UpdateUI()
+    {                
+        yield return new WaitForEndOfFrame();
+
+        if(index <= currentDialogue.GetAllNodesByIndex())
+        {
+            yield return new WaitForEndOfFrame();
+
+            StartCoroutine(WriteTextBehaviour());
+        }
+    }
+        
+    private IEnumerator WriteTextBehaviour()
+    {
+        TMP_Text textMeshProUGUI = GameObject.Find(currentNode.dialogueTextGameObject).GetComponent<TMP_Text>();
+
+        TMP_Text speakerTextMeshProGUI = GameObject.Find(currentNode.speakerTextGameObject).GetComponent<TMP_Text>();
+
+        textMeshProUGUI.color = new Color(textMeshProUGUI.color.r, textMeshProUGUI.color.g, textMeshProUGUI.color.b, 0f);
+
+        speakerTextMeshProGUI.text = currentNode.GetSpeakerText();
+
+        textMeshProUGUI.text = currentNode.GetDialogueText();
+
+        StartCoroutine(FadeInBehaviour(textMeshProUGUI));
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    //Fades in the text at the start of every node.
+    private IEnumerator FadeInBehaviour(TMP_Text text)
+    {
+        Color objectColor = text.color;
+
+        while(objectColor.a <= 1f)
+        {
+            objectColor.a += 0.2f * Time.deltaTime;
+
+            text.color = objectColor;
+
+            yield return null;
+        }
+    }
+
+    //Fades in the dialogue bubble at the start of dialoguing.
+    private IEnumerator FadeInImageBehaviour(Image image)
+    {
+        Color objectColor = image.color;
+
+        if(objectColor.a != 0f)
+        {
+            objectColor.a = 0f;
+        }
+
+        while(objectColor.a <= 0.9f)
+        {
+            objectColor.a += 5f * Time.deltaTime;
+
+            image.color = objectColor;
+
+            yield return null;
+        }
+    }
+
+    //Fades out the dialogue bubble at the end of dialoguing.
+    private IEnumerator FadeOutImageBehaviour(Image image)
+    {
+        Color objectColor = image.color;
+
+        if(objectColor.a != 1f)
+        {
+            objectColor.a = 1f;
+        }
+
+        while(objectColor.a >= 0f)
+        {
+            objectColor.a -= 5f * Time.deltaTime;
+
+            image.color = objectColor;
+
+            yield return null;
+        }
+    }
+    #endregion
+}
---
+++ the-magical-one/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+public class Dialogue : ScriptableObject, ISerializationCallbackReceiver
+{
+    #region Attributes
+    [SerializeField] List<DialogueNode> nodes = new List<DialogueNode>();
+
+    Dictionary<string, DialogueNode> nodeLookup = new Dictionary<string, DialogueNode>();
+
+    [SerializeField] Vector2 newNodeOffset = new Vector2(250, 0);
+    #endregion
+
+    //Is called when a value is changed in the inspector or when a scriptable object is loaded.
+    private void OnValidate()
+    {
+        if(nodeLookup != null)
+        {
+            nodeLookup.Clear();
+        }
+
+        foreach(DialogueNode node in GetAllNodes())
+        {
+            if(node)
+            {
+                nodeLookup[node.name] = node;
+            }
+        }
+    }
+
+    public DialogueNode GetNodeByIndex(int index)
+    {
+        return nodes[index];
+    }
+
+    public int GetAllNodesByIndex()
+    {
+        return nodes.Count;
+    }
+
+    public IEnumerable<DialogueNode> GetAllNodes()
+    {
+        return nodes;
+    }
+
+    public DialogueNode GetRootNode()
+    {
+        return nodes[0];
+    }
+
+    public List<DialogueNode> GetAllChildren(DialogueNode parentNode)
+    {
+        List<DialogueNode> dialogueNodes = new List<DialogueNode>();
+            
+        foreach(string childID in parentNode.GetChildren())
+        {
+            if(nodeLookup.ContainsKey(childID))
+            {
+                dialogueNodes.Add(nodeLookup[childID]);
+            }
+        }
+
+        return dialogueNodes;
+    }
+
+    //Runs the methods if only its an Editor window.
+    #if UNITY_EDITOR
+    public void CreateNode(DialogueNode parent)
+    {
+        DialogueNode newNode = MakeNode(parent);
+
+        Undo.RegisterCreatedObjectUndo(newNode, "Created Dialogue Node.");
+
+        Undo.RecordObject(this, "Added Dialogue Node.");
+
+        AddNode(newNode);
+    }
+
+    public void DeleteNode(DialogueNode nodeToDelete)
+    {
+        Undo.RecordObject(this, "Deleted Dialogue Node.");
+
+        nodes.Remove(nodeToDelete);
+
+        OnValidate();
+
+        Undo.DestroyObjectImmediate(nodeToDelete);
+    }
+
+    private void AddNode(DialogueNode newNode)
+    {
+        nodes.Add(newNode);
+
+        OnValidate();
+    }
+
+    private DialogueNode MakeNode(DialogueNode parent)
+    {
+        DialogueNode newNode = CreateInstance<DialogueNode>();
+
+        //Where the random naming happens.
+        newNode.name = Guid.NewGuid().ToString();
+
+        if(parent)
+        {
+            parent.AddChild(newNode.name);
+
+            newNode.SetPosition(parent.GetRect().position + newNodeOffset);
+
+            newNode.SetSpeakerTextGameObject(parent.GetSpeakerTextGameObject());
+
+            newNode.SetDialogueTextGameObject(parent.GetDialogueTextGameObject());
+
+            newNode.SetSpeakerText(parent.GetSpeakerText());
+        }
+
+        return newNode;
+    }
+    #endif
+
+    public void OnBeforeSerialize()
+    {
+        #if UNITY_EDITOR
+        if(nodes.Count == 0)
+        {
+            DialogueNode newNode = MakeNode(null);
+
+            AddNode(newNode);
+        }
+
+        if(AssetDatabase.GetAssetPath(this) != "")
+        {
+            foreach(DialogueNode node in GetAllNodes())
+            {
+                if(AssetDatabase.GetAssetPath(node) == "")
+                {
+                    AssetDatabase.AddObjectToAsset(node, this);
+                }
+            }
+        }
+        #endif
+    }
+
+    #region ISerializationCallbackReceiver Methods
+    //Needed because the Script derives from ISerializationCallbackReceiver.
+    public void OnAfterDeserialize()
+    {
+    }
+    #endregion
+}
